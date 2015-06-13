4cef6e7fa73012a2fd57a7bdbcf926d89968c749
Nate Ebel <nebel@esri.com>
2015-06-13 00:07:46 -0700

    Implemented paging of search results for continuous scrolling through full result list

---
+++ app/src/main/java/com/n8/spotifystreamer/artists/ArtistSearchFragmentHelper.java
@@ -0,0 +1,282 @@
+/*
+COPYRIGHT 1995-2015 ESRI
+
+TRADE SECRETS: ESRI PROPRIETARY AND CONFIDENTIAL 
+Unpublished material - all rights reserved under the Copyright Laws of the United States.
+
+For additional information, contact: Environmental Systems Research Institute, Inc. 
+Attn: Contracts Dept 380 New York Street Redlands, California, USA 92373
+
+email: contracts@esri.com
+*/
+package com.n8.spotifystreamer.artists;
+
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Build;
+import android.os.Handler;
+import android.provider.SearchRecentSuggestions;
+import android.support.annotation.NonNull;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.helper.ItemTouchHelper;
+import android.transition.TransitionInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.n8.spotifystreamer.AndroidUtils;
+import com.n8.spotifystreamer.R;
+import com.n8.spotifystreamer.SpotifyStreamerApplication;
+import com.n8.spotifystreamer.tracks.TopTracksActivityFragment;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import kaaes.spotify.webapi.android.models.Artist;
+import kaaes.spotify.webapi.android.models.ArtistsPager;
+import retrofit.Callback;
+import retrofit.RetrofitError;
+import retrofit.client.Response;
+
+public class ArtistSearchFragmentHelper implements ArtistSearchFragmentView.Controller, ArtistsRecyclerAdapter.ArtistClickListener {
+  private static final String TRACK_FRAGMENT_TAG = "track_fragment_tag";
+
+  private static final int REQUEST_LIMIT = 20;
+
+  public static final String SUGGESTIONS_PREFERENCES_KEY = "suggestions_preferences_key";
+
+  private ArtistSearchFragmentView mView;
+
+  private List<Artist> mArtists;
+
+  private ArtistsRecyclerAdapter mAdapter;
+
+  private LinearLayoutManager mLayoutManager;
+
+  private String mCurrentQuery;
+
+  private int mCurrentQueryOffset = 0;
+
+  private int mTotalCurrentSearchResults;
+
+  private boolean mPagingNewResults;
+
+  private FragmentActivity mActivity;
+
+  public ArtistSearchFragmentHelper(FragmentActivity activity) {
+    mActivity = activity;
+  }
+
+  public void onCreateView(@NonNull ArtistSearchFragmentView view){
+    mView = view;
+    // If view is being recreated after a rotation, there may be existing artist data to view
+    if (mArtists != null && mArtists.size() > 0) {
+      bindArtists(false);
+    }
+  }
+
+  @Override
+  public LinearLayoutManager getLinearLayoutManager() {
+    mLayoutManager = new LinearLayoutManager(mActivity);
+    return mLayoutManager;
+  }
+
+  @Override
+  public void onClearSuggestions() {
+    clearSearchSuggestions();
+  }
+
+  @Override
+  public void onSubmitQuery(String query) {
+    submitQuery(query);
+  }
+
+  @Override
+  public boolean onSuggestionClicked(int index) {
+    Set<String> suggestions = getSharedPreferences().getStringSet(SUGGESTIONS_PREFERENCES_KEY, null);
+    if (suggestions != null) {
+      submitQuery((String) suggestions.toArray()[index]);
+    }
+    return true;
+  }
+
+  @Override
+  public void onRecyclerViewScrolled(RecyclerView recyclerView, int dx, int dy) {
+    if (mAdapter.getItemCount() != mTotalCurrentSearchResults) {
+      if (mLayoutManager.findLastVisibleItemPosition() == mAdapter.getItemCount() - 1 && !mPagingNewResults) {
+
+        mView.getProgressBar().setVisibility(View.VISIBLE);
+        final Handler handler = new Handler();
+
+        mPagingNewResults = true;
+        searchForArtists(null, handler);
+      }
+    }
+  }
+
+  private void submitQuery(String query) {
+    mCurrentQuery = query;
+    mTotalCurrentSearchResults = 0;
+    mCurrentQueryOffset = 0;
+    mArtists = null;
+    mAdapter = null;
+
+    // Leverage provider to populate suggestions adapter
+    //
+    SearchRecentSuggestions suggestions = new SearchRecentSuggestions(mActivity,
+        ArtistSuggestionProvider.AUTHORITY, ArtistSuggestionProvider.MODE);
+    suggestions.saveRecentQuery(query, null);
+
+    // Use shared preferences to track suggestions so I can handle suggestion clicks without
+    // going through activity.
+    //
+    SharedPreferences sharedPreferences = getSharedPreferences();
+    Set<String> savedQueries = sharedPreferences.getStringSet(SUGGESTIONS_PREFERENCES_KEY, new HashSet<String>());
+    savedQueries.add(query);
+    sharedPreferences.edit().putStringSet(SUGGESTIONS_PREFERENCES_KEY, savedQueries).apply();
+
+    final ProgressDialog progressDialog = new ProgressDialog(mActivity);
+    progressDialog.setTitle(mActivity.getString(R.string.searching_for) + query);
+    progressDialog.setCancelable(false);
+    progressDialog.setCanceledOnTouchOutside(false);
+    progressDialog.show();
+
+    final Handler handler = new Handler();
+    searchForArtists(progressDialog, handler);
+  }
+
+  private void searchForArtists(final ProgressDialog progressDialog,
+      final Handler handler) {
+
+    Map<String, Object> queryMap = new HashMap<>();
+    queryMap.put("offset", mCurrentQueryOffset);
+    queryMap.put("limit", REQUEST_LIMIT);
+
+    SpotifyStreamerApplication.getSpotifyService().searchArtists(mCurrentQuery, queryMap,
+        new Callback<ArtistsPager>() {
+          @Override
+          public void success(final ArtistsPager artistsPager, Response response) {
+            if (progressDialog != null) {
+              progressDialog.cancel();
+            }
+
+            handler.post(new Runnable() {
+              @Override
+              public void run() {
+                mCurrentQueryOffset += REQUEST_LIMIT;
+                mTotalCurrentSearchResults = artistsPager.artists.total;
+                if (mPagingNewResults) {
+                  mPagingNewResults = false;
+                }
+                mView.getProgressBar().setVisibility(View.GONE);
+
+                handleQueryResponse(artistsPager);
+              }
+            });
+          }
+
+          @Override
+          public void failure(RetrofitError error) {
+            handler.post(new Runnable() {
+              @Override
+              public void run() {
+                if (progressDialog != null) {
+                  progressDialog.cancel();
+                }
+                mView.getProgressBar().setVisibility(View.GONE);
+
+                AndroidUtils.showToast(mActivity,
+                    mActivity.getString(R.string.error_searching_for_artist));
+              }
+            });
+          }
+        });
+  }
+
+  private SharedPreferences getSharedPreferences() {
+    return mActivity
+        .getSharedPreferences(ArtistSearchFragment.class.getSimpleName(), Context.MODE_PRIVATE);
+  }
+
+  public void onArtistViewClicked(Artist artist, ImageView sharedImageView) {
+    // If using api 22 or better, use a shared element transition.  For some reason api 21
+    // devices are displaying some odd behavior with the transition element.
+    //
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP_MR1) {
+      Fragment fragment = TopTracksActivityFragment.getInstance(artist);
+      fragment.setSharedElementEnterTransition(TransitionInflater.from(mActivity)
+          .inflateTransition(R.transition.artists_to_tracks_transition));
+      fragment.setSharedElementReturnTransition(TransitionInflater.from(mActivity)
+          .inflateTransition(R.transition.artists_to_tracks_transition));
+
+      // Add Fragment B
+      FragmentTransaction ft = mActivity.getSupportFragmentManager().beginTransaction()
+          .replace(R.id.main_activity_fragment_frame, fragment, TRACK_FRAGMENT_TAG)
+          .addToBackStack(null)
+          .addSharedElement(sharedImageView, mActivity.getString(R.string.artist_thumbnail_transition_name));
+      ft.commit();
+
+    }
+    else {
+      Fragment fragment = TopTracksActivityFragment.getInstance(artist);
+
+      // Add Fragment B
+      FragmentTransaction ft = mActivity.getSupportFragmentManager().beginTransaction()
+          .replace(R.id.main_activity_fragment_frame, fragment, TRACK_FRAGMENT_TAG)
+          .addToBackStack(null);
+      ft.commit();
+    }
+  }
+
+  private void clearSearchSuggestions(){
+    SearchRecentSuggestions suggestions = new SearchRecentSuggestions(mActivity, ArtistSuggestionProvider.AUTHORITY,
+        ArtistSuggestionProvider.MODE);
+    suggestions.clearHistory();
+
+    SharedPreferences sharedPreferences = getSharedPreferences();
+    sharedPreferences.edit().remove(SUGGESTIONS_PREFERENCES_KEY).apply();
+  }
+
+  private void handleQueryResponse(ArtistsPager artistsPager) {
+    if (mArtists == null) {
+      mArtists = artistsPager.artists.items;
+    } else {
+      int startIndex = mArtists.size();
+      mArtists.addAll(artistsPager.artists.items);
+      mAdapter.notifyItemRangeInserted(startIndex, startIndex + REQUEST_LIMIT);
+      return;
+    }
+    bindArtists(true);
+    mView.getSearchView().clearFocus();
+  }
+
+  private void bindArtists(boolean bindFromScratch) {
+    if (mArtists == null || mArtists.size() == 0) {
+      mView.showNoContentView();
+      return;
+    }
+
+    if (bindFromScratch) {
+      mAdapter = new ArtistsRecyclerAdapter(mArtists, this);
+    }
+    mView.getArtistRecyclerView().setAdapter(mAdapter);
+    setupSwipeToDeleteHelper(mView.getArtistRecyclerView());
+    mView.showContentView();
+  }
+
+  private void setupSwipeToDeleteHelper(ViewGroup view) {
+    ArtistRecyclerViewTouchHelperCallback callback = new ArtistRecyclerViewTouchHelperCallback(
+        view, mAdapter, mArtists);
+    ItemTouchHelper itemTouchHelper = new ItemTouchHelper(callback);
+    itemTouchHelper.attachToRecyclerView(mView.getArtistRecyclerView());
+  }
+}
+
---
+++ app/src/main/java/com/n8/spotifystreamer/artists/ArtistSearchFragmentView.java
@@ -0,0 +1,202 @@
+/*
+COPYRIGHT 1995-2015 ESRI
+
+TRADE SECRETS: ESRI PROPRIETARY AND CONFIDENTIAL 
+Unpublished material - all rights reserved under the Copyright Laws of the United States.
+
+For additional information, contact: Environmental Systems Research Institute, Inc. 
+Attn: Contracts Dept 380 New York Street Redlands, California, USA 92373
+
+email: contracts@esri.com
+*/
+package com.n8.spotifystreamer.artists;
+
+import android.annotation.TargetApi;
+import android.app.Activity;
+import android.app.SearchManager;
+import android.content.Context;
+import android.os.Build;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.SearchView;
+import android.support.v7.widget.Toolbar;
+import android.util.AttributeSet;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+
+import com.n8.spotifystreamer.DividerItemDecoration;
+import com.n8.spotifystreamer.R;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+import kaaes.spotify.webapi.android.models.Artist;
+
+public class ArtistSearchFragmentView extends FrameLayout {
+
+  public interface Controller {
+    LinearLayoutManager getLinearLayoutManager();
+
+    void onClearSuggestions();
+
+    void onSubmitQuery(String query);
+
+    boolean onSuggestionClicked(int index);
+
+    void onRecyclerViewScrolled(RecyclerView recyclerView, int dx, int dy);
+  }
+
+  @InjectView(R.id.fragment_artist_search_initial_content_layout)
+  View mInitialContentView;
+
+  @InjectView(R.id.fragment_artist_search_no_content_layout)
+  View mNoContentView;
+
+  @InjectView(R.id.fragment_artist_search_recyclerView)
+  RecyclerView mArtistRecyclerView;
+
+  @InjectView(R.id.fragment_artist_search_toolbar)
+  Toolbar mToolbar;
+
+  @InjectView(R.id.fragment_artis_search_searchView)
+  SearchView mSearchView;
+
+  @InjectView(R.id.fragment_artist_search_progressBar)
+  ProgressBar mProgressBar;
+
+  private Controller mController;
+
+  public ArtistSearchFragmentView(Context context) {
+    super(context);
+  }
+
+  public ArtistSearchFragmentView(Context context, AttributeSet attrs) {
+    super(context, attrs);
+  }
+
+  public ArtistSearchFragmentView(Context context, AttributeSet attrs, int defStyleAttr) {
+    super(context, attrs, defStyleAttr);
+  }
+
+  @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+  public ArtistSearchFragmentView(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+    super(context, attrs, defStyleAttr, defStyleRes);
+  }
+
+  public void setController(@NonNull Activity activity, @NonNull Controller controller) {
+    mController = controller;
+    ButterKnife.inject(this);
+
+    setuptToolbar();
+    setupSearchView(activity);
+    setupRecyclerView();
+  }
+
+  public ProgressBar getProgressBar() {
+    return mProgressBar;
+  }
+
+  public SearchView getSearchView() {
+    return mSearchView;
+  }
+
+  public Toolbar getToolbar() {
+    return mToolbar;
+  }
+
+  public RecyclerView getArtistRecyclerView() {
+    return mArtistRecyclerView;
+  }
+
+  public void showNoContentView(){
+    mInitialContentView.setVisibility(View.GONE);
+    mArtistRecyclerView.setVisibility(View.GONE);
+    mNoContentView.setVisibility(View.VISIBLE);
+  }
+
+  public void showContentView(){
+    mArtistRecyclerView.setVisibility(View.VISIBLE);
+    mNoContentView.setVisibility(View.GONE);
+    mInitialContentView.setVisibility(View.GONE);
+  }
+
+  private void setuptToolbar() {
+    mToolbar.inflateMenu(R.menu.fragment_artist_search_menu);
+    mToolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+      @Override
+      public boolean onMenuItemClick(MenuItem item) {
+        switch (item.getItemId()) {
+          case R.id.fragment_artist_search_menu_clear_suggestions:
+            mController.onClearSuggestions();
+            return true;
+          default:
+        }
+        return false;
+      }
+    });
+  }
+
+  private void setupSearchView(@NonNull Activity activity) {
+    mSearchView.setIconifiedByDefault(false);
+
+    SearchManager searchManager = (SearchManager) activity.getSystemService(
+        Context.SEARCH_SERVICE);
+    mSearchView.setSearchableInfo(searchManager.getSearchableInfo(activity.getComponentName()));
+
+    // Get the SearchView close button so I can set a custom click listener
+    View closeButton = mSearchView.findViewById(R.id.search_close_btn);
+
+    // Set own click listener because calling setOnCloseListener on the SearchView doesn't work
+    closeButton.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        mSearchView.setQuery("", false);
+        mSearchView.clearFocus();
+      }
+    });
+
+    mSearchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+      @Override
+      public boolean onQueryTextSubmit(String query) {
+        mController.onSubmitQuery(query);
+        return true;
+      }
+
+      @Override
+      public boolean onQueryTextChange(String newText) {
+        return false;
+      }
+    });
+
+    mSearchView.setOnSuggestionListener(new SearchView.OnSuggestionListener() {
+      @Override
+      public boolean onSuggestionSelect(int position) {
+        return false;
+      }
+
+      @Override
+      public boolean onSuggestionClick(int position) {
+        return mController.onSuggestionClicked(position);
+      }
+    });
+  }
+
+  private void setupRecyclerView() {
+    mArtistRecyclerView.addItemDecoration(new DividerItemDecoration(getContext(), DividerItemDecoration.VERTICAL_LIST));
+
+    final LinearLayoutManager manager = mController.getLinearLayoutManager();
+    mArtistRecyclerView.setLayoutManager(manager);
+    mArtistRecyclerView.setHasFixedSize(true);
+    mArtistRecyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
+      @Override
+      public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
+        super.onScrolled(recyclerView, dx, dy);
+
+        mController.onRecyclerViewScrolled(recyclerView, dx, dy);
+      }
+    });
+  }
+}
